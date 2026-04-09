95d17f7a385ef15f6ff0aca6fc95b69eec3b34b9
Burger <2335162068@qq.com>
2026-04-09 23:43:09 +0800

    first

--- ex06/sketch_mar26a/sketch_mar26a.ino
+++ ex06/sketch_mar26a/sketch_mar26a.ino
@@ -1,6 +1,11 @@
    // ex06 警车双闪（双通道反相PWM）
+<<<<<<< HEAD
+const int LED_A = 4;   // 第一个LED
+const int LED_B = 17;  // 第二个LED
+=======
 const int LED_A = 9;   // 第一个LED
 const int LED_B = 10;  // 第二个LED
+>>>>>>> 1a534836466a89f576a342df067d170f2915191d
 
 void setup() {
   pinMode(LED_A, OUTPUT);
